05db3c82c0ca315a94b7e98c62b8e8135e6567a7
Bryan Gallardo <bryanjgallardo@gmail.com>
2023-02-21 14:45:48 -0800

    typo fix in init function

--- modules/Client/ExampleServiceClient.lua
+++ modules/Client/ExampleServiceClient.lua
@@ -7,7 +7,7 @@ local require = require(script.Parent.loader).load(script)
 local ExampleServiceClient = {}
 
 function ExampleServiceClient:Init(serviceBag)
-	self._serviceBag = assert(self, "No serviceBag")
+	self._serviceBag = assert(serviceBag, "No serviceBag")
 
 	print("ExampleServiceClient:Init() finished!")
 end

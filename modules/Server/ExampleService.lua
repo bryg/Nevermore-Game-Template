0cb8555bb07a05775933fbfa119b4f63625a6f7b
Bryan Gallardo <bryanjgallardo@gmail.com>
2023-02-21 04:30:35 -0800

    Comment clean up.

--- modules/Server/ExampleService.lua
+++ modules/Server/ExampleService.lua
@@ -3,12 +3,9 @@
 ]=]
 local require = require(script.Parent.loader).load(script)
 
-print("Server: Hello from ExampleService!")
-
 local ExampleService = {}
 
 function ExampleService:Init(serviceBag)
-    print("Server: ExampleService:Init() started!")
     self._serviceBag = assert(serviceBag, "No serviceBag")
 
     print("Server: ExampleService:Init() finished!")
